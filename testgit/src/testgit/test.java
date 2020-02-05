439dcb0e0921f74a72ea22e2fcbf2a64805cfe8a
Mephistofer <Mephistof@163.com>
2020-02-05 11:15:49 +0800

    first commit

---
+++ testgit/src/testgit/test.java
@@ -0,0 +1,12 @@
+package testgit;
+
+public class test {
+	public static void main(String[] arg) {
+		String[] greeting = new String[3];
+		greeting[0] = "Welcome";
+		greeting[1] = "by Saya";
+		greeting[2] = "and lucky";
+		for (String g : greeting)
+			System.out.println(g);
+	}
+}
